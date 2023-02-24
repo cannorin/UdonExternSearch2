af2592f27af2b5b1a948a9b07f9ea771262d6fb2
cannorin <cannorin@users.noreply.github.com>
2023-02-24 22:17:14 +0900

    Update CI (6)

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -8,6 +8,10 @@ dotnet vpm install templates
 dotnet vpm new project World -p .
 dotnet vpm add package com.vrchat.worlds -p project
 
+# Run openupm-cli to install Cinemachine
+npm install -g openupm-cli
+cd project && openupm add com.unity.cinemachine@2.8.0
+
 # Build unity project
 unity-editor -nographics -projectPath ./project -buildWindowsPlayer /dev/null -logfile
 
--- scripts/post_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Run vpm to install the latest VRC SDK
-dotnet tool restore
-dotnet vpm add package com.vrchat.worlds -p project

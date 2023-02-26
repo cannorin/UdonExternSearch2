6f0282e18b4bd3aa40dd20888213c3851b5af858
cannorin <cannorin@users.noreply.github.com>
2023-02-26 16:53:17 +0900

    Update CI (13)

---
+++ scripts/restore_vpm_packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR/../
+
+rm project/Packages/com.vrchat.core.vpm-resolver/package.json
+dotnet vpm add package com.vrchat.core.vpm-resolver -p project
+
+rm project/Packages/com.vrchat.base/package.json
+dotnet vpm add package com.vrchat.base -p project
+
+rm project/Packages/com.vrchat.worlds/package.json
+dotnet vpm add package com.vrchat.worlds -p project
+
+rm project/Packages/com.vrchat.clientsim/package.json
+dotnet vpm add package com.vrchat.clientsim -p project
